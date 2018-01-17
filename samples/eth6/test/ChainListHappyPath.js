31d46bfc880b2770da685778d5199b74b7ea7c6f
Diego <dmenin@gmail.com>
2018-01-17 13:38:49 +0000

    running on private net

--- samples/eth6/test/ChainListHappyPath.js
+++ samples/eth6/test/ChainListHappyPath.js
@@ -8,6 +8,7 @@ contract('ChainList', function(accounts) {
   var articleName = "article 1";
   var articleDescription = "Description for article 1";
   var articlePrice = 10;
+  var watcher;
 
   // Test case: check initial values
   it("should be initialized with empty values", function() {
@@ -35,6 +36,24 @@ contract('ChainList', function(accounts) {
       assert.equal(data[1], articleName, "article name must be " + articleName);
       assert.equal(data[2], articleDescription, "article descriptio must be " + articleDescription);
       assert.equal(data[3].toNumber(), web3.toWei(articlePrice, "ether"), "article price must be " + web3.toWei(articlePrice, "ether"));
+    });
+  });
+
+  // Test case: should check events
+  it("should trigger an event when a new article is sold", function() {
+    return ChainList.deployed().then(function(instance) {
+      chainListInstance = instance;
+      watcher = chainListInstance.sellArticleEvent();
+      return chainListInstance.sellArticle(
+        articleName,
+        articleDescription,
+        web3.toWei(articlePrice, "ether"), {from: seller}
+      );
+    }).then(function(receipt) {
+      assert.equal(receipt.logs.length, 1, "should have received one event");
+      assert.equal(receipt.logs[0].args._seller, seller, "seller must be " + seller);
+      assert.equal(receipt.logs[0].args._name, articleName, "article name must be " + articleName);
+      assert.equal(receipt.logs[0].args._price.toNumber(), web3.toWei(articlePrice, "ether"), "article price must be " + web3.toWei(articlePrice, "ether"));
     });
   });
 });
